c607904550ab8ea1eb9b3fa9cc503ceb93afc0a7
Castiel-Tyler <castieltyleraiko@gmail.com>
2025-09-29 11:40:59 -0300

    criação de js para os usuarios

--- usuario.js
+++ usuario.js
@@ -1,13 +1,31 @@
 const API_URL = "http://localhost:3001/filmes";
+const filmeCardsContainer = document.getElementById("filmeCardsContainer")
 
 
 
+function fetchAndRenderFilme() {
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(filme => renderFilme(filme))
+        .catch(error => {
+            console.error('Erro ao buscar filme', error),
+                filmeCardsContainer.innerHTML = `<p class="p modal modal-content">Erro ao carregar filmes!</p>`;
+        })
+    }
+    //criação de cards na div via js
+function renderFilme(filme) {
+    filmeCardsContainer.innerHTML = '';
+
+    if (filme.length === 0) {
+        filmeCardsContainer.innerHTML = `<p>Nenhum filme encontrado</p>`;
+        return;
+    }
 filme.forEach(filme => {
     const filmeCard = document.createElement('div');
     filmeCard.className = 'filme-card'
 
     filmeCard.innerHTML = `
-            <div class="filme-info">
+            <div class="filme">
                 <img src="${filme.urlCapa}"alt="Capa do filme ${filme.titulo}" class="filme-capa"></img>
                 <p><strong>ID:</strong>${filme.id}</p>
                 <p><strong>Titulo:</strong>${filme.titulo}</p>
@@ -15,9 +33,9 @@ filme.forEach(filme => {
                 <p><strong>Categoria:</strong>${filme.categoria}</p>
                 <p><strong>Ano:</strong>${filme.anoLancamento}</p>
             </div>
-            <div class="card-buttons">
-                <button class="btn-edit">Editar</button>
-                <button class="btn-delete">Excluir</button>
-            </div>
         `;
 })
+    filmeCardsContainer.appendChild(filmeCard)
+}
+
+fetchAndRenderFilme();
