b3f0cde8abef9c3d00c23111885aaa06d6eb1a33
Benio <benioesteveaqui@gmail.com>
2025-09-30 08:37:58 -0300

    remoção de capas

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.use(cors())
 const PORT = 3001;
 
 let filmes = [
-    { id: 1, titulo: "Weapons", diretor: "Zach Cregger", anoLancamento: 2025, urlCapa: "https://m.media-amazon.com/images/S/pv-target-images/762870abd32c3d64c7cabb9775dbee4a83d3d9f73f923d2a2bd531cc4a27cc0d.jpg", categoria: "Horror" },
-    { id: 2, titulo: "Superman", diretor: "James Gunn", anoLancamento: 2025, urlCapa: "https://m.media-amazon.com/images/S/pv-target-images/c077277fb805649bb6785aabe2472a706b8e51a8dd629533a95cac955778d65a.jpg", categoria: "Action/Sci-Fi" },
-    { id: 3, titulo: "Fantastic Four: First Steps", diretor: "Matt Shakman", anoLancamento: 2025, urlCapa: "https://m.media-amazon.com/images/M/MV5BOGM5MzA3MDAtYmEwMi00ZDNiLTg4MDgtMTZjOTc0ZGMyNTIwXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", categoria: "Action/Sci-Fi" },
-    { id: 4, titulo: "Batman Ninja vs. Yakuza League", diretor: "Junpei Mizusaki & Shinji Takagi", anoLancamento: 2025, urlCapa: "https://m.media-amazon.com/images/S/pv-target-images/d49dc4d058ade082d087c827891a5903d62faa7fef26a793a9da9607d7d551ac.jpg", categoria: "Action/Sci-Fi" },
-    { id: 5, titulo: "Kimetsu no Yaiba: Mugen Castle", diretor: "Haruo Sotozaki & Hikaru Kondo", anoLancamento: 2025, urlCapa: "https://maceioshopping.com/app/uploads/2025/09/demon-slayer-kimetsu-no-yaiba-infinity-castle.jpg", categoria: "Action/Adventure" }
+    { id: 1, titulo: "Weapons", diretor: "Zach Cregger", anoLancamento: 2025, urlCapa: "", categoria: "Horror" },
+    { id: 2, titulo: "Superman", diretor: "James Gunn", anoLancamento: 2025, urlCapa: "", categoria: "Action/Sci-Fi" },
+    { id: 3, titulo: "Fantastic Four: First Steps", diretor: "Matt Shakman", anoLancamento: 2025, urlCapa: "", categoria: "Action/Sci-Fi" },
+    { id: 4, titulo: "Batman Ninja vs. Yakuza League", diretor: "Junpei Mizusaki & Shinji Takagi", anoLancamento: 2025, urlCapa: "", categoria: "Action/Sci-Fi" },
+    { id: 5, titulo: "Kimetsu no Yaiba: Mugen Castle", diretor: "Haruo Sotozaki & Hikaru Kondo", anoLancamento: 2025, urlCapa: "", categoria: "Action/Adventure" }
 ]
 
 app.get('/', (req, res) => {
